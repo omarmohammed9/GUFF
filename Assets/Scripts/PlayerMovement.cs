eb8dd7cb38991fe1fa5aba3f7e01687fd7b6874c
Omar Mohammed <omar_mohammed9@hotmail.com>
2019-11-13 14:58:37 -0700

    Updates

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -41,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
 	void MoveCharacter()
 	{
 		myRigidbody.MovePosition(
-			transform.position + change * speed * Time.deltaTime
+			transform.position + change * speed * Time.fixedDeltaTime
 		);
 	}
 }
