d3e198a6afd5a96bdab4fb0bbcc9ead542652846
Omar Mohammed <omar_mohammed9@hotmail.com>
2019-11-14 22:27:54 -0700

    Breakable objects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState{
+	walk,
+	attack,
+	interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
+	public PlayerState currentState;
 	public float speed;
 	private Rigidbody2D myRigidbody;
 	private Vector3 change;
 	private Animator animator;
+	
     // Start is called before the first frame update
     void Start()
     {
+		currentState = PlayerState.walk;
 		animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+		animator.SetFloat("moveX", 0);
+		animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -21,8 +32,25 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
 		change.x = Input.GetAxis("Horizontal");
 		change.y = Input.GetAxis("Vertical");
-		UpdateAnimationAndMove();
+		if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+		{
+			StartCoroutine(AttackCo());
+		}
+		else if(currentState == PlayerState.walk)
+		{
+			UpdateAnimationAndMove();
+		}
     }
+	
+	private IEnumerator AttackCo()
+	{
+		animator.SetBool("attacking", true);
+		currentState = PlayerState.attack;
+		yield return null;
+		animator.SetBool("attacking", false);
+		yield return new WaitForSeconds(.3f);
+		currentState = PlayerState.walk;
+	}
 	
 	void UpdateAnimationAndMove()
 	{
@@ -40,6 +68,7 @@ public class PlayerMovement : MonoBehaviour
 	
 	void MoveCharacter()
 	{
+		change.Normalize();
 		myRigidbody.MovePosition(
 			transform.position + change * speed * Time.fixedDeltaTime
 		);
